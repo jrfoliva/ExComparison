7b226f47d0d1b2e374a5beb4864b8eb7d4d9da04
Junior Oliva <jrfoliva@gmail.com>
2023-10-16 22:14:01 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ExComparison.Entities;
+
+namespace System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+            products.Sort();
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
